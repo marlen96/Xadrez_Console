ad3f78838bd7309fdcf1ce6c26157f2b500f143b
Iury Santiago Rodrigues <iury.contato96@gmail.com>
2018-03-30 12:36:43 -0300

    Criado a posição do tabuleiro

--- Xadrez_Console/Program.cs
+++ Xadrez_Console/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Tabuleiro;
 
 namespace Xadrez_Console
 {
@@ -10,6 +7,10 @@ namespace Xadrez_Console
     {
         static void Main(string[] args)
         {
+            Posicao p = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + p);
+            Console.ReadLine();
         }
     }
 }
